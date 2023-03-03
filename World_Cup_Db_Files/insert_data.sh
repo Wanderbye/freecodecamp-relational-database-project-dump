5054fbf6481a3e0817132e2814c63d831bddbdc1
Wanderbye <125409160+Wanderbye@users.noreply.github.com>
2023-03-02 21:50:47 -0600

    Update insert_data.sh

--- World_Cup_Db_Files/insert_data.sh
+++ World_Cup_Db_Files/insert_data.sh
@@ -38,3 +38,4 @@ do
   # insert the games into games table
   INSERT_GAME_RESULT=$($PSQL "INSERT INTO games(year, round, winner_id, opponent_id, winner_goals, opponent_goals) VALUES('$YEAR', '$ROUND', '$WINNER_ID', '$OPPONENT_ID', '$WINNER_GOALS', '$OPPONENT_GOALS')")
 done
+
